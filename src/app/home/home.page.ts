0e4ed7304febbeda1358618a78e97742811681c7
Dain <dain.park@my.jcu.edu.au>
2019-08-31 21:53:09 +0800

    edit home and login page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LoginPage } from '../login/login.page';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,7 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  
+  
 
 }
