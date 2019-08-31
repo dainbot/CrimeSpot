de8ab021adcaaf4cf485f97adf8012fa46b73b4a
jefffry <54535025+jefffry@users.noreply.github.com>
2019-08-31 18:44:10 +0800

    solve merge conflict

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,24 +3,12 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},  { path: 'crime-report', loadChildren: './crime-report/crime-report.module#CrimeReportPageModule' },
-
-<<<<<<< HEAD
-];
-
-@NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
-=======
-const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   { path: 'maps', loadChildren: './maps/maps.module#MapsPageModule' },
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
+  { path: 'crime-report', loadChildren: './crime-report/crime-report.module#CrimeReportPageModule' },
+
+  
 ];
 
 @NgModule({
@@ -30,4 +18,3 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
->>>>>>> faf6c9a7a625bed48a4e6ed8a77ea4db0994a3af
