5e119d27997f24750ead66b7fec74e6bc70bd057
Dain <dain.park@my.jcu.edu.au>
2019-09-01 14:26:08 +0800

    overrited app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,32 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AngularFireModule } from 'angularfire2';
+import { environment } from '../environments/environment';
+import { AngularFireAuthModule } from "angularfire2/auth";
+import { AngularFireDatabaseModule } from "angularfire2/database";
+import { AngularFireStorageModule } from 'angularfire2/storage';
+
+
+var firebaseConfig= {
+  apiKey: "AIzaSyB3aTRrV46Pp1SN9FgoYBtyfBJHWMSGw8c",
+  authDomain: "crimespot-7e597.firebaseapp.com",
+  databaseURL: "https://crimespot-7e597.firebaseio.com",
+  projectId: "crimespot-7e597",
+  storageBucket: "",
+  messagingSenderId: "35057494887",
+}
 
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  
+  imports: [BrowserModule, IonicModule.forRoot(),
+    AppRoutingModule, AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule,
+    AngularFireStorageModule
+  ],
   providers: [
     StatusBar,
     SplashScreen,
